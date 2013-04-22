a8ff428dab8d6dda1e750b4550dd8186e541babb
Angel Matas <perodebuenrollo@hotmail.com>
2013-04-22 18:24:53 +0200

    create the class ResourcesManager and implement the methods of splashScene

--- src/org/escoladeltreball/universerescue/ResourcesManager.java
+++ src/org/escoladeltreball/universerescue/ResourcesManager.java
@@ -2,7 +2,9 @@ package org.escoladeltreball.universerescue;
 
 import org.andengine.engine.Engine;
 import org.andengine.engine.camera.Camera;
+import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.atlas.bitmap.BuildableBitmapTextureAtlas;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
@@ -12,19 +14,19 @@ public class ResourcesManager {
 	
 	//Attributes
 	private static  ResourcesManager obj = null;
-	private GameActivity activity;
-	private Camera camera;
-	private Engine engine;
-	private VertexBufferObjectManager vbom;
-	private BitmapTextureAtlas splashTextureAtlas;
-	private BuildableBitmapTextureAtlas menuTextureAtlas;
-	private ITextureRegion splash_region;
-	private ITextureRegion menu_background_region;
+	public GameActivity activity;
+	public Camera camera;
+	public Engine engine;
+	public VertexBufferObjectManager vbom;
+	public BitmapTextureAtlas splashTextureAtlas;
+	public BuildableBitmapTextureAtlas menuTextureAtlas;
+	public ITextureRegion splash_region;
+	public ITextureRegion menu_background_region;
 	
 	
 	//Singleton
 	
-	public ResourcesManager getInstance(){
+	public static ResourcesManager getInstance(){
 		if(obj == null){
 			obj = new ResourcesManager();
 		}
@@ -41,11 +43,23 @@ public class ResourcesManager {
 	 * @param vbom
 	 * 
 	 */
-	public void prepareManager(Engine engine, Camera camera, GameActivity activity, VertexBufferObjectManager vbom){
+	public static void setup(Engine engine, Camera camera, GameActivity activity, VertexBufferObjectManager vbom){
     	getInstance().engine = engine;
     	getInstance().camera = camera;
     	getInstance().activity = activity;
     	getInstance().vbom = vbom;
+	}
+	
+	public void loadSplashScreen(){
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+		splashTextureAtlas = new BitmapTextureAtlas(activity.getTextureManager(), 200, 200,TextureOptions.BILINEAR);
+		splash_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(splashTextureAtlas, activity, "logoUR.png",0,0);
+		splashTextureAtlas.load();
+	}
+	
+	public void unloadSplashScreen(){
+		splashTextureAtlas.unload();
+		splash_region = null;
 	}
 	
 	
