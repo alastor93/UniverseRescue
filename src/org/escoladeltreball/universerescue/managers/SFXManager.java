b4c3c2508626dc1070cf555fc88d6bec8f02e8ba
José Luis Mas <jlmaro93@gmail.com>
2013-04-25 19:56:04 +0200

    Include music in the menu

--- src/org/escoladeltreball/universerescue/managers/SFXManager.java
+++ src/org/escoladeltreball/universerescue/managers/SFXManager.java
@@ -1,6 +1,11 @@
 package org.escoladeltreball.universerescue.managers;
 
+import java.io.IOException;
+
 import org.andengine.audio.music.Music;
+import org.andengine.audio.music.MusicFactory;
+import org.andengine.util.debug.Debug;
+import org.escoladeltreball.universerescue.GameActivity;
 
 public class SFXManager {
 	
@@ -16,7 +21,58 @@ public class SFXManager {
 		return obj;
 	}
 
+	public SFXManager() {
+		MusicFactory.setAssetBasePath("sounds/");
+		try {
+			music  = MusicFactory.createMusicFromAsset(ResourcesManager.getInstance().activity.getMusicManager() ,ResourcesManager.getInstance().activity, "music.ogg");
+			music.setLooping(true);
+		} catch (IOException e) {
+			Debug.e(e);
+		}
+	}	
 	
+	public boolean isSoundMuted() {
+		return soundsMuted;
+	}
 	
+	public static boolean isMusicMuted() {
+		return getInstance().musicMuted;
+	}
+	
+	public static void setMusicMuted(final boolean pMuted) {
+		getInstance().musicMuted = pMuted;
+		if(getInstance().musicMuted) music.pause(); else music.play();
+		GameActivity.writeIntToSharedPreferences(GameActivity.SHARED_PREFS_MUSIC_MUTED, (getInstance().musicMuted? 1:0));
+	}
+	
+	public static boolean toggleMusicMuted() {
+		getInstance().musicMuted = !getInstance().musicMuted;
+		if(getInstance().musicMuted) music.pause(); else music.play();
+		GameActivity.writeIntToSharedPreferences(GameActivity.SHARED_PREFS_MUSIC_MUTED, (getInstance().musicMuted? 1:0));
+		return getInstance().musicMuted;
+	}
+	
+	public static void playMusic() {
+		if(!isMusicMuted())
+			music.play();
+	}
+	
+	public static void pauseMusic() {
+		music.pause();
+	}
+	
+	public static void resumeMusic() {
+		if(!isMusicMuted())
+			music.resume();
+	}
+	
+	public static float getMusicVolume() {
+		return music.getVolume();
+	}
+	
+	public static void setMusicVolume(final float pVolume) {
+		music.setVolume(pVolume);
+	
+	}
 
 }
