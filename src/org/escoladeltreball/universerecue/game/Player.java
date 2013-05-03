45dcfab9e231c27c64178441657bbc26908d32fb
José Luis Mas <jlmaro93@gmail.com>
2013-05-03 18:18:19 +0200

    create Player class and the method run

---
+++ src/org/escoladeltreball/universerecue/game/Player.java
@@ -0,0 +1,43 @@
+package org.escoladeltreball.universerecue.game;
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.entity.sprite.vbo.ITiledSpriteVertexBufferObject;
+import org.andengine.extension.physics.box2d.PhysicsConnector;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
+import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.opengl.texture.region.ITiledTextureRegion;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+
+public class Player extends AnimatedSprite {
+	private Body dynamicBody;
+	private boolean isJump;
+
+	public Player(float pX, float pY, ITiledTextureRegion pTiledTextureRegion,
+			ITiledSpriteVertexBufferObject pTiledSpriteVertexBufferObject,
+			Camera camera, PhysicsWorld physicsWorld) {
+		super(pX, pY, pTiledTextureRegion, pTiledSpriteVertexBufferObject);
+		this.createPhysics(camera, physicsWorld);
+		camera.setChaseEntity(this);
+	}
+
+	private void createPhysics(Camera camera, PhysicsWorld physicsWorld) {
+		dynamicBody = PhysicsFactory.createBoxBody(physicsWorld, this,
+				BodyType.DynamicBody, PhysicsFactory.createFixtureDef(0, 0, 0));
+		dynamicBody.setUserData("player");
+		dynamicBody.setFixedRotation(true);
+		physicsWorld.registerPhysicsConnector(new PhysicsConnector(this,
+				dynamicBody,true,false));
+	}
+
+	public void jump() {
+
+	}
+
+	public void run(float pValueX) {
+		dynamicBody.setLinearVelocity(pValueX * 10,
+				dynamicBody.getLinearVelocity().y);
+	}
+}
