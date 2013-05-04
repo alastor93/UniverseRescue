4c7466cf2557e1a69d2f3cb141fdebd9f64376d2
José Luis Mas <jlmaro93@gmail.com>
2013-05-04 16:32:28 +0200

    Created the limits of game

---
+++ src/org/escoladeltreball/universerescue/game/Wall.java
@@ -0,0 +1,24 @@
+package org.escoladeltreball.universerescue.game;
+
+import org.andengine.entity.primitive.Rectangle;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
+import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+
+public class Wall extends Rectangle {
+	private Body staticBody;
+
+	public Wall(float pX, float pY, float pWidth, float pHeight,
+			VertexBufferObjectManager pVertexBufferObjectManager,PhysicsWorld mPhysicsWorld) {
+		super(pX, pY, pWidth, pHeight, pVertexBufferObjectManager);
+		this.createPhysics(mPhysicsWorld);
+	}
+	
+	private void createPhysics(PhysicsWorld mPhysicsWorld){
+		staticBody = PhysicsFactory.createBoxBody(mPhysicsWorld,this, BodyType.StaticBody,PhysicsFactory.createFixtureDef(0, 0, 0));
+	}
+
+}
