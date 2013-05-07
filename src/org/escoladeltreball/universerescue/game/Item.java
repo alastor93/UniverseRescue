41ebc8c202b03c61e7594fba561a082c243137ed
Angel Matas <perodebuenrollo@hotmail.com>
2013-05-07 21:29:36 +0200

    add item class

--- src/org/escoladeltreball/universerescue/game/Item.java
+++ src/org/escoladeltreball/universerescue/game/Item.java
@@ -1,23 +1,22 @@
 package org.escoladeltreball.universerescue.game;
 
 import org.andengine.engine.camera.Camera;
-import org.andengine.entity.sprite.AnimatedSprite;
-import org.andengine.entity.sprite.vbo.ITiledSpriteVertexBufferObject;
+import org.andengine.entity.sprite.Sprite;
 import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
-import org.andengine.opengl.texture.region.ITiledTextureRegion;
+import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
-public class Item extends AnimatedSprite{
+public class Item extends Sprite{
 	
 	private Body itemBody;
 
 	public Item(float pX, float pY,
-			ITiledTextureRegion pTiledTextureRegion,
+			ITextureRegion pTiledTextureRegion,
 			VertexBufferObjectManager pVertexBufferObjectManager,
 			Camera camera, PhysicsWorld physicsWorld) {
 		super(pX, pY, pTiledTextureRegion, pVertexBufferObjectManager);
@@ -27,7 +26,7 @@ public class Item extends AnimatedSprite{
 	
 	private void createPhysics(Camera camera, PhysicsWorld physicsWorld) {
 		itemBody = PhysicsFactory.createBoxBody(physicsWorld, this,
-				BodyType.KinematicBody, PhysicsFactory.createFixtureDef(0, 0, 0));
+				BodyType.DynamicBody, PhysicsFactory.createFixtureDef(0, 0, 0));
 		physicsWorld.registerPhysicsConnector(new PhysicsConnector(this,
 				itemBody, true, false));
 	}
