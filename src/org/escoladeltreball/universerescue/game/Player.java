1306b6ec10d6142145fe9ff4125c7a4332f6ca3d
José Luis Mas <jlmaro93@gmail.com>
2013-05-07 14:41:36 +0200

    implements fire and fixed jump for jump only one time

--- src/org/escoladeltreball/universerescue/game/Player.java
+++ src/org/escoladeltreball/universerescue/game/Player.java
@@ -1,24 +1,33 @@
 package org.escoladeltreball.universerescue.game;
 
 import org.andengine.engine.camera.Camera;
+import org.andengine.entity.primitive.Rectangle;
+import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.extension.physics.box2d.util.Vector2Pool;
 import org.andengine.opengl.texture.region.ITiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Manifold;
 
 public class Player extends AnimatedSprite {
 	private Body dynamicBody;
+	private Body bulletBody;
 	private boolean isJump;
 
 	public Player(float pX, float pY, ITiledTextureRegion pTiledTextureRegion,
-			VertexBufferObjectManager VertexBufferObject,
-			Camera camera, PhysicsWorld physicsWorld) {
+			VertexBufferObjectManager VertexBufferObject, Camera camera,
+			PhysicsWorld physicsWorld) {
 		super(pX, pY, pTiledTextureRegion, VertexBufferObject);
 		this.setScale(2);
 		this.createPhysics(camera, physicsWorld);
@@ -31,11 +40,56 @@ public class Player extends AnimatedSprite {
 		dynamicBody.setUserData("player");
 		dynamicBody.setFixedRotation(true);
 		physicsWorld.registerPhysicsConnector(new PhysicsConnector(this,
-				dynamicBody,true,false));
+				dynamicBody, true, false));
+		physicsWorld.setContactListener(new ContactListener() {
+
+			@Override
+			public void preSolve(Contact contact, Manifold oldManifold) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void postSolve(Contact contact, ContactImpulse impulse) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void endContact(Contact contact) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void beginContact(Contact contact) {
+				isJump = false;
+			}
+		});
 	}
 
 	public void jump() {
-		dynamicBody.setLinearVelocity(new Vector2(0,4));
+		if (!isJump) {
+			dynamicBody.setLinearVelocity(new Vector2(0, 4));
+			isJump = true;
+		}
+	}
+
+	public void fire(Scene scene, PhysicsWorld physicsWorld) {
+		Rectangle bullet = new Rectangle(this.getX() + 20, this.getHeight() / 2,
+				10, 10, this.getVertexBufferObjectManager());
+		final Vector2 velocity = Vector2Pool.obtain(10, 0);
+		bullet.setRotation(this.getRotation());
+
+		final FixtureDef bulletFixtureDef1 = PhysicsFactory.createFixtureDef(0,
+				0, 0);
+		this.bulletBody = PhysicsFactory.createBoxBody(physicsWorld, bullet,
+				BodyType.KinematicBody, bulletFixtureDef1);
+		this.bulletBody.setLinearVelocity(velocity);
+		Vector2Pool.recycle(velocity);
+		physicsWorld.registerPhysicsConnector(new PhysicsConnector(bullet,
+				this.bulletBody, true, false));
+		scene.attachChild(bullet);
 	}
 
 	public void run(float pValueX) {
