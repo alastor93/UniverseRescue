c2fef4cb474875602f2bae8e8dabf6d520cfd562
José Luis Mas <jlmaro93@gmail.com>
2013-05-07 19:25:28 +0200

    Modify fire to fire in several directions

--- src/org/escoladeltreball/universerescue/game/Player.java
+++ src/org/escoladeltreball/universerescue/game/Player.java
@@ -4,12 +4,15 @@ import org.andengine.engine.camera.Camera;
 import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.entity.sprite.Sprite;
 import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
 import org.andengine.extension.physics.box2d.util.Vector2Pool;
 import org.andengine.opengl.texture.region.ITiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.util.math.MathUtils;
+import org.escoladeltreball.universerescue.managers.ResourcesManager;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -24,6 +27,13 @@ public class Player extends AnimatedSprite {
 	private Body dynamicBody;
 	private Body bulletBody;
 	private boolean isJump;
+	private double directionX;
+	private double directionY;
+
+	public void setDirection(double directionX,double directionY) {
+		this.directionX = directionX;
+		this.directionY = directionY;
+	}
 
 	public Player(float pX, float pY, ITiledTextureRegion pTiledTextureRegion,
 			VertexBufferObjectManager VertexBufferObject, Camera camera,
@@ -36,7 +46,7 @@ public class Player extends AnimatedSprite {
 
 	private void createPhysics(Camera camera, PhysicsWorld physicsWorld) {
 		dynamicBody = PhysicsFactory.createBoxBody(physicsWorld, this,
-				BodyType.DynamicBody, PhysicsFactory.createFixtureDef(0, 0, 0));
+				BodyType.DynamicBody, PhysicsFactory.createFixtureDef(0, 0, 1));
 		dynamicBody.setUserData("player");
 		dynamicBody.setFixedRotation(true);
 		physicsWorld.registerPhysicsConnector(new PhysicsConnector(this,
@@ -70,15 +80,30 @@ public class Player extends AnimatedSprite {
 
 	public void jump() {
 		if (!isJump) {
-			dynamicBody.setLinearVelocity(new Vector2(0, 4));
+			dynamicBody.setLinearVelocity(new Vector2(dynamicBody
+					.getLinearVelocity().y, 6));
 			isJump = true;
 		}
 	}
 
 	public void fire(Scene scene, PhysicsWorld physicsWorld) {
-		Rectangle bullet = new Rectangle(this.getX() + 20, this.getHeight() / 2,
-				10, 10, this.getVertexBufferObjectManager());
-		final Vector2 velocity = Vector2Pool.obtain(10, 0);
+		Sprite bullet = new Sprite(this.getX() + 60, this.getY(),
+				ResourcesManager.getInstance().bulletSprite,
+				this.getVertexBufferObjectManager());
+		Vector2 velocity = Vector2Pool.obtain(10, 0);
+		if (directionX < 0) {
+			bullet = new Sprite(this.getX() - 60, this.getY(),
+					ResourcesManager.getInstance().bulletSprite,
+					this.getVertexBufferObjectManager());
+			bullet.setFlippedHorizontal(true);
+			velocity = Vector2Pool.obtain(-10, 0);
+		} else if (directionY > 0) {
+			bullet = new Sprite(this.getX(), this.getY() + 60,
+					ResourcesManager.getInstance().bulletSprite,
+					this.getVertexBufferObjectManager());
+			bullet.setFlipped(true, true);
+			velocity = Vector2Pool.obtain(0, 10);
+		}
 		bullet.setRotation(this.getRotation());
 
 		final FixtureDef bulletFixtureDef1 = PhysicsFactory.createFixtureDef(0,
@@ -95,5 +120,6 @@ public class Player extends AnimatedSprite {
 	public void run(float pValueX) {
 		dynamicBody.setLinearVelocity(pValueX * 10,
 				dynamicBody.getLinearVelocity().y);
+		this.animate(300, true);
 	}
 }
