6f0ed4d6525adf765f0727cb3247a75301a4b2f7
Jose Luis Martin Ordoñez <jsee.martin@gmail.com>
2013-05-10 01:42:21 +0200

    Added CoolDown class for manage delay

---
+++ src/org/escoladeltreball/universerescue/game/CoolDown.java
@@ -0,0 +1,40 @@
+package org.escoladeltreball.universerescue.game;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class CoolDown {
+	private boolean valid;
+	private Timer timer;
+	private long delay = 500;
+	private static CoolDown instance = null;
+
+	public static CoolDown getInstance() {
+		if (instance == null) {
+			instance = new CoolDown();
+		}
+		return instance;
+	}
+
+	private CoolDown() {
+		timer = new Timer();
+		valid = true;
+	}
+
+	public boolean canShoot() {
+		if (valid) {
+			valid = false;
+			timer.schedule(new Task(), delay);
+			return true;
+		}
+		return false;
+	}
+
+	class Task extends TimerTask {
+
+		public void run() {
+			valid = true;
+		}
+
+	}
+}
