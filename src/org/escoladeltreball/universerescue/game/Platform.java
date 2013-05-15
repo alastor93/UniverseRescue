d0627dc06d13e903d76d5e20c70f8fafb026b10d
Angel Matas <perodebuenrollo@hotmail.com>
2013-05-15 18:09:17 +0200

    Delete class PlatformMoveX and add all in platform with new Constructor

--- src/org/escoladeltreball/universerescue/game/Platform.java
+++ src/org/escoladeltreball/universerescue/game/Platform.java
@@ -6,6 +6,7 @@ import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
 import org.andengine.opengl.texture.region.ITiledTextureRegion;
+import org.andengine.opengl.util.GLState;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
 import com.badlogic.gdx.physics.box2d.Body;
@@ -13,24 +14,49 @@ import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 public class Platform extends AnimatedSprite {
 	private Body bdBody;
-
-
+	private boolean movePlatform;
+	private float finX = this.getX();
+	private boolean back;
 
 	public Platform(float pX, float pY,
 			ITiledTextureRegion pTiledTextureRegion,
 			VertexBufferObjectManager pVertexBufferObjectManager,
-			Camera camera, PhysicsWorld physicsWorld) {
+			Camera camera, PhysicsWorld physicsWorld, boolean movePlat) {
 		super(pX, pY, pTiledTextureRegion, pVertexBufferObjectManager);
 		this.createPhysics(camera, physicsWorld);
-		//this.animate(200);
+		movePlatform = movePlat;
+		// this.animate(200);
 	}
 
 	private void createPhysics(Camera camera, PhysicsWorld physicsWorld) {
 		bdBody = PhysicsFactory.createBoxBody(physicsWorld, this,
-				BodyType.KinematicBody, PhysicsFactory.createFixtureDef(0, 0, 0));
+				BodyType.KinematicBody,
+				PhysicsFactory.createFixtureDef(0, 0, 0));
 		bdBody.setUserData("platform");
 		physicsWorld.registerPhysicsConnector(new PhysicsConnector(this,
 				bdBody, true, false));
+	}
+
+	@Override
+	protected void onManagedUpdate(float pSecondsElapsed) {
+		if (movePlatform) {
+			if (finX < 600 && !back) {
+				finX += 1;
+				bdBody.setLinearVelocity(1.7f, 0);
+			} else if (finX == 400) {
+				back = false;
+			} else if (finX == 600 || back) {
+				finX -= 1;
+				back = true;
+				bdBody.setLinearVelocity(-1.7f, 0);
+			}
+		}
+	}
+
+	@Override
+	protected void preDraw(GLState pGLState, Camera pCamera) {
+		super.preDraw(pGLState, pCamera);
+		pGLState.setDitherEnabled(true);
 	}
 	
 	
