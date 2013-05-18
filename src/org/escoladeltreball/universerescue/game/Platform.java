ea28ea6c4293ce21131f4ad6ce9b3be3cfff3336
José Luis Mas <jlmaro93@gmail.com>
2013-05-18 16:05:00 +0200

    Added Two methods to move platforms left to right and backwards

--- src/org/escoladeltreball/universerescue/game/Platform.java
+++ src/org/escoladeltreball/universerescue/game/Platform.java
@@ -1,29 +1,30 @@
 package org.escoladeltreball.universerescue.game;
 
 import org.andengine.engine.camera.Camera;
-import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.entity.sprite.Sprite;
 import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
-import org.andengine.opengl.texture.region.ITiledTextureRegion;
+import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.util.GLState;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
-public class Platform extends AnimatedSprite {
+public class Platform extends Sprite {
 	private Body bdBody;
 	private float finX = this.getX();
 	private boolean back;
 
 	public Platform(float pX, float pY,
-			ITiledTextureRegion pTiledTextureRegion,
+			ITextureRegion pTextureRegion,
 			VertexBufferObjectManager pVertexBufferObjectManager,
 			Camera camera, PhysicsWorld physicsWorld) {
-		super(pX, pY, pTiledTextureRegion, pVertexBufferObjectManager);
+		super(pX, pY, pTextureRegion, pVertexBufferObjectManager);
+		this.setScale(2);
+		this.setCullingEnabled(true);
 		this.createPhysics(camera, physicsWorld);
-		// this.animate(200);
 	}
 
 	private void createPhysics(Camera camera, PhysicsWorld physicsWorld) {
@@ -36,18 +37,31 @@ public class Platform extends AnimatedSprite {
 	}
 
 	
-	public void moveX(){
-			if (finX < 600 && !back) {
-				finX += 1;
-				bdBody.setLinearVelocity(1.7f, 0);
-			} else if (finX == 400) {
-				back = false;
-			} else if (finX == 600 || back) {
+	public void moveRightToLeft(int init,int limit){
+			if (finX > limit && !back) {
 				finX -= 1;
-				back = true;
 				bdBody.setLinearVelocity(-1.7f, 0);
+			} else if (finX == init) {
+				back = false;
+			} else if (finX == limit || back) {
+				finX += 1;
+				back = true;
+				bdBody.setLinearVelocity(1.7f, 0);
 			}
 	}
+	
+	public void moveLeftToRight(int init,int limit){
+		if (finX < limit && !back) {
+			finX += 1;
+			bdBody.setLinearVelocity(1.7f, 0);
+		} else if (finX == init) {
+			back = false;
+		} else if (finX == limit || back) {
+			finX -= 1;
+			back = true;
+			bdBody.setLinearVelocity(-1.7f, 0);
+		}
+}
 
 	@Override
 	protected void preDraw(GLState pGLState, Camera pCamera) {
