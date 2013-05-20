f5fd6d5bb97bfa0e5afb94be9cdbe538465d8a1d
angelmatas <angelmatas@dhcppc3>
2013-05-20 17:33:26 +0200

    add class Stalactite

---
+++ src/org/escoladeltreball/universerescue/game/Stalactite.java
@@ -0,0 +1,55 @@
+package org.escoladeltreball.universerescue.game;
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.sprite.vbo.ISpriteVertexBufferObject;
+import org.andengine.extension.physics.box2d.PhysicsConnector;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
+import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.escoladeltreball.universerescue.managers.ResourcesManager;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+
+public class Stalactite extends Sprite {
+
+	private Body stalactiteBody;
+	private PhysicsWorld physicsWorld;
+	private PhysicsConnector physicsConnector;
+
+	public Stalactite(float pX, float pY, ITextureRegion pTiledTextureRegion,
+			VertexBufferObjectManager pVertexBufferObjectManager,
+			Camera camera, PhysicsWorld physicsWorld) {
+		super(pX, pY, pTiledTextureRegion, pVertexBufferObjectManager);
+		this.physicsWorld = physicsWorld;
+		this.createPhysics(camera);
+	}
+
+	private void createPhysics(Camera camera) {
+		stalactiteBody = PhysicsFactory.createBoxBody(physicsWorld, this,
+				BodyType.DynamicBody, PhysicsFactory.createFixtureDef(0, 0, 0));
+		stalactiteBody.setUserData("item");
+		physicsConnector = new PhysicsConnector(this, stalactiteBody, true,
+				false);
+		physicsWorld.registerPhysicsConnector(physicsConnector);
+	}
+
+	public void removeStalac() {
+		ResourcesManager.getInstance().activity
+				.runOnUpdateThread(new Runnable() {
+
+					@Override
+					public void run() {
+						physicsWorld
+								.unregisterPhysicsConnector(physicsConnector);
+						stalactiteBody.setActive(false);
+						physicsWorld.destroyBody(stalactiteBody);
+						detachSelf();
+						System.gc();
+					}
+				});
+	}
+
+}
