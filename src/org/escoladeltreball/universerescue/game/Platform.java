05c8ca3232b924915cb5b3ebb6596b09d60973c4
José Luis Mas <jlmaro93@gmail.com>
2013-05-22 20:39:18 +0200

    Change the collision of platforms

--- src/org/escoladeltreball/universerescue/game/Platform.java
+++ src/org/escoladeltreball/universerescue/game/Platform.java
@@ -5,10 +5,12 @@ import org.andengine.entity.sprite.Sprite;
 import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.extension.physics.box2d.util.constants.PhysicsConstants;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.util.GLState;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
@@ -16,18 +18,26 @@ public class Platform extends Sprite {
 	private Body bdBody;
 	private float finX = this.getX();
 	private boolean back;
+	private final float width;
+	private final float height;
 
 	public Platform(float pX, float pY, ITextureRegion pTextureRegion,
 			VertexBufferObjectManager pVertexBufferObjectManager,
 			Camera camera, PhysicsWorld physicsWorld) {
 		super(pX, pY, pTextureRegion, pVertexBufferObjectManager);
 		this.setScale(2);
-		this.setCullingEnabled(true);
+		this.width = this.getWidth() * this.getScaleX()
+				/ PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT;
+		this.height = this.getHeight() * this.getScaleY()
+				/ PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT;
 		this.createPhysics(camera, physicsWorld);
 	}
 
 	private void createPhysics(Camera camera, PhysicsWorld physicsWorld) {
-		bdBody = PhysicsFactory.createBoxBody(physicsWorld, this,
+		Vector2[] vector2s = {
+				new Vector2(+0.50000f * width, +0.52941f * height),
+				new Vector2(-0.50000f * width, +0.52941f * height) };
+		bdBody = PhysicsFactory.createPolygonBody(physicsWorld, this, vector2s,
 				BodyType.KinematicBody,
 				PhysicsFactory.createFixtureDef(0, 0, 0));
 		bdBody.setUserData("platform");
