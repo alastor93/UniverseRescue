1d98352b4bf212fad5ff8d11dbba7b183d8afa19
angelmatas <angelmatas@localhost.localdomain>
2013-05-26 17:33:45 +0200

    add comments in wall

--- src/org/escoladeltreball/universerescue/game/Wall.java
+++ src/org/escoladeltreball/universerescue/game/Wall.java
@@ -9,14 +9,20 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 public class Wall extends Rectangle {
+	//Attributes
 	private Body staticBody;
-
+	
+	//Constructor
 	public Wall(float pX, float pY, float pWidth, float pHeight,
 			VertexBufferObjectManager pVertexBufferObjectManager,PhysicsWorld mPhysicsWorld) {
 		super(pX, pY, pWidth, pHeight, pVertexBufferObjectManager);
 		this.createPhysics(mPhysicsWorld);
 	}
 	
+	/**
+	 * Create the physics of the wall
+	 * @param mPhysicsWorld
+	 */
 	private void createPhysics(PhysicsWorld mPhysicsWorld){
 		staticBody = PhysicsFactory.createBoxBody(mPhysicsWorld,this, BodyType.StaticBody,PhysicsFactory.createFixtureDef(0, 0, 0));
 		staticBody.setUserData("wall");
