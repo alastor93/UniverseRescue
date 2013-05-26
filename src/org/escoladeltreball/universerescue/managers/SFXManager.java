16670ce186317c88178ceb529687fda930c2107a
angelmatas <angelmatas@localhost.localdomain>
2013-05-26 13:51:59 +0200

    add comments in SFXmanager class

--- src/org/escoladeltreball/universerescue/managers/SFXManager.java
+++ src/org/escoladeltreball/universerescue/managers/SFXManager.java
@@ -10,12 +10,13 @@ import org.andengine.util.debug.Debug;
 import org.escoladeltreball.universerescue.GameActivity;
 
 public class SFXManager {
-
+	//Attributes
 	private static SFXManager obj = null;
 	private static Music music;
 	public boolean musicMuted;
 	private static Sound shoot;
 
+	//Singleton
 	public static SFXManager getInstance() {
 		if (obj == null) {
 			obj = new SFXManager();
@@ -23,6 +24,7 @@ public class SFXManager {
 		return obj;
 	}
 
+	//Constructor
 	public SFXManager() {
 		MusicFactory.setAssetBasePath("sounds/");
 		try {
@@ -43,11 +45,18 @@ public class SFXManager {
 			Debug.e(e);
 		}
 	}
-
+	/**
+	 * Check if the music is muted
+	 * @return true if is muted false otherwhise
+	 */
 	public static boolean isMusicMuted() {
 		return getInstance().musicMuted;
 	}
-
+	
+	/**
+	 * pause or play the music depends of the param
+	 * @param pMuted
+	 */
 	public static void setMusicMuted(final boolean pMuted) {
 		getInstance().musicMuted = pMuted;
 		if (getInstance().musicMuted)
@@ -59,6 +68,10 @@ public class SFXManager {
 				(getInstance().musicMuted ? 1 : 0));
 	}
 
+	/**
+	 * Check if the music is muted
+	 * @return the vakue of the var musicMuted
+	 */
 	public static boolean toggleMusicMuted() {
 		getInstance().musicMuted = !getInstance().musicMuted;
 		if (getInstance().musicMuted)
@@ -71,35 +84,60 @@ public class SFXManager {
 		return getInstance().musicMuted;
 	}
 
+	/**
+	 * Play the music
+	 */
 	public static void playMusic() {
 		if (!isMusicMuted()) {
 			music.play();
 		}
 	}
-
+	/**
+	 * Pause the music
+	 */
 	public static void pauseMusic() {
 		music.pause();
 	}
-
+	/**
+	 * Resume the music
+	 */
 	public static void resumeMusic() {
 		if (!isMusicMuted()) {
 			music.resume();
 		}
 	}
 
+	/**
+	 * Get the volume of the music
+	 * @return volume of music
+	 */
 	public static float getMusicVolume() {
 		return music.getVolume();
 	}
 
+	/**
+	 * Set the volume of the music
+	 * @param pVolume
+	 */
 	public static void setMusicVolume(final float pVolume) {
 		music.setVolume(pVolume);
 
 	}
-
+	/**
+	 * Play the sound of the weapon
+	 * @param pRate
+	 * @param pVolume
+	 */
 	public static void playShoot(final float pRate, final float pVolume) {
 		playSound(shoot, pRate, pVolume);
 	}
-
+	
+	/**
+	 * Play the sound of the parameter
+	 * @param pSound
+	 * @param pRate
+	 * @param pVolume
+	 */
 	private static void playSound(final Sound pSound, final float pRate,
 			final float pVolume) {
 		if (!isMusicMuted()) {
